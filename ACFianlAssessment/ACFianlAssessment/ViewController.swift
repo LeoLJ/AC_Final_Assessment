489cd33ec6ba714dee8a32aafa9078be49d0fbaa
LeoLJ <ljliu@cycu.org.tw>
2016-08-04 17:55:03 +0800

    updat Q3

--- ACFianlAssessment/ACFianlAssessment/ViewController.swift
+++ ACFianlAssessment/ACFianlAssessment/ViewController.swift
@@ -7,12 +7,47 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+ 
+        //MARK Q3 GET origin
+        Alamofire.request(.GET, "https://httpbin.org/get").validate().responseJSON {
+            response in
+            switch response.result {
+            case .Success:
+                if let data = response.result.value {
+                    let json = JSON(data)
+                    NSLog(String(json["origin"]))
+                }
+            case .Failure(let error):
+                print(error)
+            }
+        }
+        
+        //MARK Q3 POST
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd' 'HH:mm:ss"
+        let currentDate = formatter.stringFromDate(NSDate())
+        let parameters = ["time":"\(currentDate)"]
+        Alamofire.request(.POST, "https://httpbin.org/post", parameters: parameters).validate().responseJSON {
+            response in
+            switch response.result {
+            case .Success:
+                if let data = response.result.value {
+                    let json = JSON(data)
+                    let sendingTime = formatter.dateFromString(String(json["form"]["time"]))
+                    let gap = NSString(format:"%.4f", -(sendingTime?.timeIntervalSinceNow)!)
+                    NSLog("spent almost \(gap) sec" )
+                }
+            case .Failure(let error):
+                print(error)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
---
+++ ACFianlAssessment/Pods/Target Support Files/Pods-ACFianlAssessment/Pods-ACFianlAssessment-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ACFianlAssessment : NSObject
+@end
+@implementation PodsDummy_Pods_ACFianlAssessment
+@end
